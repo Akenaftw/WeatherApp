73d88d08ed148630c3a8b01e77936b37d335eff4
Akena <felix.j.c.vanroost.com>
2020-09-17 17:17:19 +0200

    added images but bricked page will correct this tomorrow

--- weather-app/script.js
+++ weather-app/script.js
@@ -3,6 +3,7 @@ let createApi = () => {
     let cityName = document.getElementById("location").value;
     let APIKey = config;
     let fetchAdress = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=metric&APPID=" + APIKey;
+    console.log(fetchAdress)
     fetchweather(fetchAdress);
 
 }
@@ -10,6 +11,7 @@ let createApi = () => {
 document.getElementById("trigger").addEventListener("click", createApi)
 var weather = {
     date: "",
+    image: "",
     weather: "",
     temperatureMax: "",
     temperatureMin: "",
@@ -22,14 +24,14 @@ function fetchweather(url) {
     fetch(url).then(res => res.json()).then(data => {
         console.log(data)
         for (var i = 0; i < 6; i++) {
-            weather.date = today(i);
+            weather.image = data["list"][i]["weather"][0]["icon"];
             weather.temperatureMax = data["list"][i]["main"]["temp_max"];
             weather.temperatureMin = data["list"][i]["main"]["temp_min"];
             weather.temperatureAverage = data["list"][i]["main"]["temp"];
             weather.humidity = data["list"][i]["main"]["humidity"];
             weather.windSpeed = data["list"][i]["wind"]["speed"];
             console.log(JSON.stringify(weather));
-            displayWeather(weather);
+            today(i,weather)
             }
 
     })
@@ -39,33 +41,29 @@ function fetchweather(url) {
 
 }
 function displayWeather(data){
-    let day = today;
     let extraDay = document.createElement("div");
     let target = document.getElementById("target")
     extraDay.classList.add("days");
     extraDay.innerHTML =
-        data.day +
-        "<br>max temperature= " + data.temperatureMax +
+        data.date +
+        "<br> <img src=\"http://openweathermap.org/img/wn/"+ data.image +"@2x.png\">
+        "<br> Max-temperature= " + data.temperatureMax +
         "<br> Min-temperature= " + data.temperatureMin +
         "<br> Average temperature = "+ data.temperatureAverage +
         "<br> humidity = " + data.humidity +
         "<br> windspeed = " + data.windSpeed + ".";
     target.appendChild(extraDay);
-
-}
-
-Date.prototype.getDayOfWeek = function(){
-    return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][ this.getDay() ];
-    console.log(this.getDay())
 };
-function today(i) {
+
+function today(i,weather) {
     var today = new Date()
     var dd = today.getDate()+ i;
     var mm = today.getMonth();
     var yyyy = today.getFullYear();
     today= dd + "/" + mm + "/" + yyyy;
-
-    return today;
+    weather.date = today;
+    displayWeather(weather);
+    console.log(today);
 }
 
 
