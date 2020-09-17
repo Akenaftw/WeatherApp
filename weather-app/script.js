524c71c90c977108474483d51aa569c36fc911df
Akena <felix.j.c.vanroost.com>
2020-09-17 11:25:31 +0200

    got the data from the api to display properly on my html page, next up is styling so it doesnt look like a blob of text

--- weather-app/script.js
+++ weather-app/script.js
@@ -14,24 +14,38 @@ var weather = {
     temperatureAverage: "",
     humidity: "",
     windSpeed:"",
-
 }
 
 function fetchweather(url) {
     fetch(url).then(res => res.json()).then(data => {
         console.log(data)
-        for (let i = 0; i < 6; i++) {
+        for (var i = 0; i < 6; i++) {
             weather.temperatureMax = data["list"][i]["main"]["temp_max"];
             weather.temperatureMin = data["list"][i]["main"]["temp_min"];
             weather.temperatureAverage = data["list"][i]["main"]["temp"];
             weather.humidity = data["list"][i]["main"]["humidity"];
             weather.windSpeed = data["list"][i]["wind"]["speed"];
             console.log(JSON.stringify(weather));
-        }
+            displayWeather(weather);
+            }
+
     })
         .catch(function (error) {
             console.log(error)
         })
+
+}
+function displayWeather(data){
+    let extraDay = document.createElement("div");
+    let target = document.getElementById("target")
+    extraDay.classList.add("days");
+    extraDay.innerHTML =
+        "max temperature= " + data.temperatureMax +
+        "<br> Min-temperature= " + data.temperatureMin +
+        "<br> Average temperature = "+ data.temperatureAverage +
+        "<br> humidity = " + data.humidity +
+        "<br> windspeed = " + data.windSpeed + ".";
+    target.appendChild(extraDay);
 
 }
 
